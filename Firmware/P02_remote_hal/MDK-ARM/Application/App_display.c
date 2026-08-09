40d64cc1c7557e1e77af4c7b6b1ac66f8c998d02
Alan <149546180+kfcmmm123@users.noreply.github.com>
2026-08-09 17:19:35 -0400

    OLED display task completed

--- Firmware/P02_remote_hal/MDK-ARM/Application/App_display.c
+++ Firmware/P02_remote_hal/MDK-ARM/Application/App_display.c
@@ -1,5 +1,15 @@
 #include "App_display.h"
 
+extern Remote_Data remote_data;
+
+void App_display_show_bar(uint8_t x, uint8_t y, uint8_t count)
+{
+    if (count < 13)
+    {
+        OLED_Show_CH(x, y, 12 + count, 12, 1);
+    }
+}
+
 /**
  * @brief  Initialize the display
  */
@@ -13,5 +23,72 @@ void App_display_init(void)
  */
 void App_display_show(void)
 {
-    OLED_ShowString(0, 0, "Hello, World!", 16, 1);
+    // 1st line: Show title
+    OLED_ShowString(LINE1_BEGIN, Y0, "STM32 Drone Alan", 12, 1);
+
+    // 2nd line: Show channel number
+    uint8_t buff[3] = { 0 };
+    sprintf((char*)buff, "%03d", CHANNEL);
+    OLED_ShowString(LINE2_BEGIN, Y1, buff, 12, 1);
+
+    // 3rd line: Show throttle value and bars
+    OLED_ShowString(LINE3_BEGIN, Y2, "THR:", 12, 1);
+    uint8_t count = 0;
+    if (remote_data.thr > 500) 
+    {
+        count = (remote_data.thr - 500) / 41;
+        App_display_show_bar(BAR1_BEGIN, Y2, 12);
+        App_display_show_bar(BAR2_BEGIN, Y2, count);
+    }
+    else 
+    {
+        count = remote_data.thr / 41;
+        App_display_show_bar(BAR1_BEGIN, Y2, count);
+        App_display_show_bar(BAR2_BEGIN, Y2, 0);
+    }
+    
+    OLED_ShowString(LINE3_BEGIN2, Y2, "ROL:", 12, 1);
+    if (remote_data.rol > 500) 
+    {
+        count = (remote_data.rol - 500) / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y2, 12);
+        App_display_show_bar(BAR2_BEGIN2, Y2, count);
+    }
+    else 
+    {
+        count = remote_data.rol / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y2, count);
+        App_display_show_bar(BAR2_BEGIN2, Y2, 0);
+    }
+
+    // 4th line 
+    OLED_ShowString(LINE4_BEGIN, Y3, "YAW:", 12, 1);
+    if (remote_data.yaw > 500) 
+    {
+        count = (remote_data.yaw - 500) / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y3, 12);
+        App_display_show_bar(BAR2_BEGIN2, Y3, count);
+    }
+    else 
+    {
+        count = remote_data.yaw / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y3, count);
+        App_display_show_bar(BAR2_BEGIN2, Y3, 0);
+    }
+
+    OLED_ShowString(LINE4_BEGIN2, Y3, "PIT:", 12, 1);
+    if (remote_data.pit > 500) 
+    {
+        count = (remote_data.pit - 500) / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y3, 12);
+        App_display_show_bar(BAR2_BEGIN2, Y3, count);
+    }
+    else 
+    {
+        count = remote_data.pit / 41;
+        App_display_show_bar(BAR1_BEGIN2, Y3, count);
+        App_display_show_bar(BAR2_BEGIN2, Y3, 0);
+    }
+
+    OLED_Refresh_Gram();
 }
--- Firmware/P02_remote_hal/MDK-ARM/Application/App_display.h
+++ Firmware/P02_remote_hal/MDK-ARM/Application/App_display.h
@@ -2,6 +2,26 @@
 #define __APP_DISPLAY_H__
 
 #include "Inf_OLED.h"
+#include "Int_SI24R1.h"
+#include "App_process_data.h"
+
+#define LINE1_BEGIN 28
+#define LINE2_BEGIN 5
+#define LINE3_BEGIN 5
+#define LINE4_BEGIN 5
+
+#define LINE3_BEGIN2 65
+#define LINE4_BEGIN2 65
+
+#define BAR1_BEGIN 35
+#define BAR2_BEGIN 47
+#define BAR1_BEGIN2 95
+#define BAR2_BEGIN2 107
+
+#define Y0 0
+#define Y1 14
+#define Y2 26
+#define Y3 38
 
 /**
  * @brief  Initialize the display
